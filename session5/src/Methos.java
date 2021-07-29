b6235abc1b163a9a669e5a2160b31a4ceb9d2f15
dalipdivaker <dalipdivakar@gmail.com>
2021-07-29 15:06:49 +0530

    first commit

---
+++ session5/src/Methos.java
@@ -0,0 +1,26 @@
+
+class Maths{
+	static int logic(int a, int b) {
+		int c;
+		if(a>b) {
+			c=a+b;
+		}
+		else {
+			c=(a+b)*10;
+		}
+			}
+	
+}
+
+
+
+
+public class Methos {
+	
+	
+	public static void main(String[] args) {
+ 
+Methods.logic(10, 20);
+	}
+
+}
